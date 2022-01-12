f1f161d44ab4f9da9a8aa50dab47d4f3963b9b67
ziadAlswary <91101322+ziadAlswary@users.noreply.github.com>
2022-01-12 14:38:46 +0300

    add challeng 5 file of Q 1

---
+++ ch5_q1.py
@@ -0,0 +1,8 @@
+total = 0
+while 1:
+    num = int(input("enter any number : "))
+    total += num
+    print("The total is → ",total)
+    if total > 50 : break
+    
+print("\n\n"," The total is bigger than 50 ".center(40,'_'))
